a773920c29f6158aa9346261589f95e6731ed7e7
guus <guus@dana>
2023-08-27 14:06:55 +0200

    html stuff

--- js/main.js
+++ js/main.js
@@ -7,14 +7,14 @@ const winchNew = new WinchSettings();
 winchNew.SetDefaults();
 // init 'struct' for settings remote
 const winchCurr = new WinchSettings();
-winchCurr.SetDefaults();
+// winchCurr.SetDefaults();
 
 // init obj to send all the serial commands
 const wsender = new SerialWinchSender();
 // init a serial parser from winch send the instance to edit as arg (place to put data in)
 const serialParser = new SerialWinchParser(winchCurr);
 
-// F onclick to connect to serial port(check on port connected evntlistener?) 
+// F onclick to connect to serial port (should check on port connected evntlistener?) 
 function connect() {
     if (webserial) {
         if (document.getElementById("con").value == "Connect") {
@@ -25,6 +25,86 @@ function connect() {
             document.getElementById("con").value = "Connect"
         }
     }
+}
+
+
+//obviously a very bad implementation of onchange handler, but i don't feel like doin it right in this hiddious language!
+function changeCatchAll() {
+    // fuck it, just get all data from page
+    getNewData();
+    setVisable();
+}
+
+function setVisable() {
+
+
+    switch (winchNew.mode) {
+        case WinchModes.LIFOLLOWPREV:
+            document.getElementById("NewPosB").disabled = true;
+            document.getElementById("NewPosC").disabled = true;
+
+            document.getElementById("NewUpCD").disabled = true;
+            document.getElementById("NewDownCD").disabled = true;
+
+            document.getElementById("NewDevB").disabled = false;
+            document.getElementById("NewDevC").disabled = false;
+            document.getElementById("NewDevD").disabled = false;
+            break;
+
+        case WinchModes.LIFOLLOWA:
+            document.getElementById("NewPosB").disabled = true;
+            document.getElementById("NewPosC").disabled = true;
+
+            document.getElementById("NewUpCD").disabled = true;
+            document.getElementById("NewDownCD").disabled = true;
+
+
+            document.getElementById("NewDevB").disabled = false;
+            document.getElementById("NewDevC").disabled = false;
+            document.getElementById("NewDevD").disabled = false;
+            break;
+
+        case WinchModes.LIDUBBLEPAIR:
+            document.getElementById("NewPosB").disabled = true;
+            document.getElementById("NewPosC").disabled = true;
+
+            document.getElementById("NewUpCD").disabled = false;
+            document.getElementById("NewDownCD").disabled = false;
+
+            document.getElementById("NewDevB").disabled = false;
+            document.getElementById("NewDevC").disabled = true;
+            document.getElementById("NewDevD").disabled = false;
+            break;
+
+        case WinchModes.LI4INLINE:
+            document.getElementById("NewPosC").disabled = false;
+            document.getElementById("NewPosB").disabled = false;
+
+            document.getElementById("NewUpCD").disabled = true;
+            document.getElementById("NewDownCD").disabled = true;
+
+            document.getElementById("NewDevB").disabled = true;
+            document.getElementById("NewDevC").disabled = true;
+            document.getElementById("NewDevD").disabled = false;
+            break;
+
+        case WinchModes.LI3INLINE:
+            document.getElementById("NewPosC").disabled = true;
+            document.getElementById("NewPosB").disabled = false;
+
+
+            document.getElementById("NewUpCD").disabled = true;
+            document.getElementById("NewDownCD").disabled = true;
+
+            document.getElementById("NewDevB").disabled = true;
+            document.getElementById("NewDevC").disabled = false;
+            document.getElementById("NewDevD").disabled = true;
+            break;
+
+        default:
+            break;
+    }
+
 }
 
 // copy the set data from the html to the winchNew obj
@@ -49,7 +129,24 @@ function getNewData() {
     // document.getElementById("NewPosA").innerHTML = winchCurr.WinchALinP;
     winchNew.WinchBlinP = document.getElementById("NewPosB").innerHTML;
     winchNew.WinchClinP = document.getElementById("NewPosC").innerHTML;
+
+    let mode = document.getElementById("NewMode").mode;
+    for (let i = 0; i < mode.length; i++) {
+        if (mode[i].checked == true) {
+            winchNew.mode = WinchModes[mode[i].id];
+            // console.log(WinchModes[mode[i].id])
+        }
+    };
+
+    let type = document.getElementById("NewType").type;
+    for (let i = 0; i < type.length; i++) {
+        if (type[i].checked == true) {
+            winchNew.type = WinchTypes[type[i].id];
+            // console.log(WinchTypes[type[i].id])
+        }
+    }
 }
+
 
 // copy the data from the winchNew obj to the html
 function setNewData() {
@@ -74,7 +171,6 @@ function setNewData() {
 
     document.getElementById("NewName").value = winchNew.name;
 
-
     document.getElementById("AddrIncrement").value = 40
 }
 
@@ -87,12 +183,12 @@ function setCurrentData() {
         document.getElementById("CurrAddrB").innerHTML = winchCurr.WinchBaddr;
     if (winchCurr.WinchCaddr || (winchCurr.WinchTrimUp === 0))
         document.getElementById("CurrAddrC").innerHTML = winchCurr.WinchCaddr;
-    if (winchCurr.WinchDaddr  || (winchCurr.WinchTrimUp === 0))
+    if (winchCurr.WinchDaddr || (winchCurr.WinchTrimUp === 0))
         document.getElementById("CurrAddrD").innerHTML = winchCurr.WinchDaddr;
 
     if (winchCurr.WinchTrimUp || (winchCurr.WinchTrimUp === 0))
         document.getElementById("CurrUp").innerHTML = winchCurr.WinchTrimUp;
-    if (winchCurr.WinchTrimDown  || (winchCurr.WinchTrimUp === 0))
+    if (winchCurr.WinchTrimDown || (winchCurr.WinchTrimUp === 0))
         document.getElementById("CurrDown").innerHTML = winchCurr.WinchTrimDown;
     if (winchCurr.WinchTrimUpCD || (winchCurr.WinchTrimUp === 0))
         document.getElementById("CurrUpCD").innerHTML = winchCurr.WinchTrimUpCD;
@@ -115,9 +211,49 @@ function setCurrentData() {
         document.getElementById("CurrPosC").innerHTML = winchCurr.WinchClinP;
     if (winchCurr.name)
         document.getElementById("CurrName").innerHTML = winchCurr.name;
-
-
+    // set mode and type to html
+    if (winchCurr.mode) {
+        let m;
+        switch (winchCurr.mode) {
+            case WinchModes.LIFOLLOWA:
+                m = "Square";
+                break;
+            case WinchModes.LIFOLLOWPREV:
+                m = "Triangle";
+                break;
+            case WinchModes.LIDUBBLEPAIR:
+                m = "Double ab & cd";
+                break;
+            case WinchModes.LI3INLINE:
+                m = "3-in-line";
+                break;
+            case WinchModes.LI34NLINE:
+                m = "3-in-line";
+                break;
+            default:
+                break;
+        }
+        document.getElementById("CurrMode").innerHTML = m;
+    }
+    if (winchCurr.type) {
+        let t;
+        switch (winchCurr.type) {
+            case WinchTypes.TYORBISFLY5:
+                t = "Orbi 5";
+                break;
+            case WinchTypes.TYORBISFLY9:
+                t = "Orbi 9";
+                break;
+            case WinchTypes.TYDLB:
+                t = "DLB";
+                break;
+            default:
+                break;
+        }
+        document.getElementById("CurrType").innerHTML = t;
+    }
 }
+
 
 // increases the addesses of WinchSettings Obj, 
 // takes WinchSettings obj, number to increment, bool to increment safety also
@@ -150,5 +286,8 @@ setCurrentData();
 setNewData();
 // attacht send funton to winchsender
 wsender.sendatacb = webserial.sendSerial;
-wsender.reqAllData();
-wsender.setAllData(winchNew);
+// wsender.reqAllData();
+// wsender.setAllData(winchNew);
+
+getNewData();
+setVisable();
