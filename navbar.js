129d5fda82dc5496287f9480c065aee9748a64b2
guus <guus@dana>
2024-01-31 13:57:07 +0100

    menu stuff

--- navbar.js
+++ navbar.js
@@ -1,10 +1,10 @@
 const htmltext = '  <nav>\
  <ul>\
  \
- <li> <a href="https://www.ledstructures.com">Ledstructures.com</a></li>\
- <li> <a href="../ls/program.html">Program LS</a></li>\
-        <li><a href="../mq/chamsys_uni.html">Magicq patch shift</a></li>\
-        <li><a href="../winch/winch.html">Winch controller</a></li>\
+        <li class=" " id="">           <a onclick="" href="https://www.ledstructures.com">    Ledstructures.com</a></li>\
+        <li class=" " id="">           <a onclick="" href="../ls/program.html">               Program LS</a></li>\
+        <li class=" " id="">           <a onclick="" href="../mq/chamsys_uni.html">           Magicq patch shift</a></li>\
+        <li class=" " id="">           <a onclick="" href="../winch/winch.html">              Winch controller</a></li>\
     </ul >\
     </nav>\
     ';
