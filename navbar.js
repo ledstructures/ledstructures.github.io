593fbbf45d242befc6b0edc56110fa99fc347695
guuuuus <guus.rooijers@gmail.com>
2026-01-02 01:28:45 +0100

    added pixels

--- navbar.js
+++ navbar.js
@@ -1,16 +1,23 @@
 const htmltext = '  <nav>\
  <ul>\
  \
-        <li class=" " id="">           <a onclick="" href="https://www.ledstructures.com">    Ledstructures.com</a></li>\
-        <li class=" " id="">           <a onclick="" href="../ls/program.html">               Program LS</a></li>\
-        <li class=" " id="">           <a onclick="" href="../mq/chamsys_uni.html">           Magicq patch shift</a></li>\
-        <li class=" " id="">           <a onclick="" href="../winch/winch.html">              Winch controller</a></li>\
+        <li class=" " id="lscom">           <a onclick="" href="https://www.ledstructures.com">    Ledstructures.com</a></li>\
+        <li class=" " id="programmain">           <a onclick="" href="../ls/program.html">               Program LS</a></li>\
+        <li class=" " id="pixelcalc">           <a onclick="" href="../pixel/pixelmain.html">           Pixel calculator</a></li>\
+        <li class=" " id="unimain">           <a onclick="" href="../mq/chamsys_uni.html">           Magicq patch shift</a></li>\
+        <li class=" " id="winchmain">           <a onclick="" href="../winch/winch.html">              Winch controller</a></li>\
     </ul >\
     </nav>\
     ';
 {
     let oldelem = document.querySelector("script#navbar");
     let newelem = document.createElement("div");
+
     newelem.innerHTML = htmltext;
     oldelem.parentNode.replaceChild(newelem, oldelem);
+    function higliteactive(higliteid) {
+        document.getElementById(higliteid).classList.add('menu_active');
+        console.log(higliteid);
+    }
+
 }
